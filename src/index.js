037bd4772362ef67ba8a52920ff9957390ef6862
Itamar <itamar.n@matomy.com>
2014-12-11 11:21:10 +0200

    put ad creation in a separate lib

--- src/index.js
+++ src/index.js
@@ -1,71 +1,14 @@
 (function(){
+
     var Qs      = require('qs'),
+        ads     = require('./ads.js'),
         confE   = document.getElementById("mobfoxConfig"),
         mobfoxVar   = "mobfox_" + String(Math.random()).slice(2),
-        refreshInterval;
-
-    createAd = {
-        banner : function(ad,ad_id,confElement){
-            var iframe = document.getElementById(mobfoxVar);
-            if(iframe){
-                iframe.parentNode.removeChild(iframe);
-            }
-
-            iframe = document.createElement("iframe");
-            iframe.id = ad_id;
-            iframe.className = "mobfox_iframe"; 
-            iframe.width= mobfoxConfig.width;
-            iframe.height= mobfoxConfig.height;
-            iframe.src= ["data:text/html;charset=utf-8,","<html>",ad,"</html>"].join("\n");
-            confE.parentNode.insertBefore(iframe,confElement);
-            iframe.style.margin = "0px";
-            iframe.style.padding= "0px";
-            iframe.style.border= "none";
-        },
-        interstitial : function(ad,ad_id,confElement){
-            var adContainer = document.getElementById('mobfox_interstitial');
-            if(adContainer){
-                adContainer.parentNode.removeChild(iframe);
-            }
-
-            adContainer = document.createElement('iframe'); 
-            adContainer.style.width  = window.innerWidth + "px";
-            adContainer.style.height = window.innerHeight + "px";
-            adContainer.style.zIndex = "1000000";
-            adContainer.style.backgroundColor = "transparent";
-            adContainer.style.position = "fixed";
-            adContainer.style.left = "0px";
-            adContainer.style.top = "0px";
-            adContainer.style.margin = "0px";
-            adContainer.style.padding= "0px";
-            adContainer.style.border= "none";
-            document.body.appendChild(adContainer);
-            
-            adContainer.contentWindow.document.body.style.margin = "0px";
-            var iframe = adContainer.contentWindow.document.createElement('iframe');
-            iframe.id = ad_id;
-            iframe.width= mobfoxConfig.width;
-            iframe.height= mobfoxConfig.height;
-            iframe.src= ["data:text/html;charset=utf-8,","<html>",ad,"</html>"].join("\n");
-            adContainer.contentWindow.document.body.appendChild(iframe);
-            iframe.style.margin = "0px auto";
-            iframe.style.padding= "0px";
-            iframe.style.border= "none";
-            iframe.style.display= "block";
-
-            var button = adContainer.contentWindow.document.createElement('button');
-            adContainer.contentWindow.document.body.appendChild(button);
-            button.innerHTML = "X";
-            button.onclick = function(){
-               adContainer.parentNode.removeChild(adContainer); 
-            };
-            button.style.position   = "absolute";
-            button.style.width      = "50px";
-            button.style.height     = "50px";
-            button.style.top        = "10px";
-            button.style.right      = "10px";
-        }
-    }; 
+        refreshInterval,
+        createAd = {
+            banner          : ads.createBanner,
+            interstitial    : ads.createInterstitial
+        }; 
     //-------------------------------------------
     function retrieve(){
 
