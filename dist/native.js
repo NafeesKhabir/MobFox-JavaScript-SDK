04376ae320f1f0b4924e67f42ecdee300040e574
Itamar <itamar.n@matomy.com>
2015-01-04 09:32:18 +0200

    native

--- dist/native.js
+++ dist/native.js
@@ -1696,7 +1696,7 @@ module.exports = {
             title.innerHTML         = [adData.textassets.headline,"-",adData.textassets.cta].join(" ");
             title.style.height      = "20%";
             title.style.position    = "absolute";
-            title.style.bottom      = "0px";
+            title.style.top         = "0px";
             title.style.width       = "100%";
             title.style.background  = "#fff";
             title.style.opacity     = "0.7";
@@ -1752,15 +1752,16 @@ module.exports = {
                 r_resp : 'json',
                 n_img : 'icon',
                 n_txt : 'headline',
-                s     : '80187188f458cfde788d961b6882fd53',
-                //s       : mobfoxConfig.publicationID,
+                //s     : '80187188f458cfde788d961b6882fd53',
+                s       : mobfoxConfig.publicationID,
                 //i     : '2.122.29.194',
-                i       : "8.8.8.8",
+                i       : mobfoxConfig.i || "8.8.8.8",
                 u     : 'Mozilla/5.0%20(iPhone;%20U;%20CPU%20iPhone%20OS%203_0%20like%20Mac%20OS%20X;%20en-us)%20AppleWebKit/528.18%20(KHTML,%20like%20Gecko)%20Version/4.0%20Mobile/7A341%20Safari/528.16',
                 o_iosadvid : '68753A44-4D6F-1226-9C60-0050E4C00067'
         };
    
         options.forEach(function(o){
+            console.log([o,mobfoxConfig[o]].join(" : "));
             if(typeof(mobfoxConfig[o]) !== 'undefined'){
                 params[o] = mobfoxConfig[o];
             }
