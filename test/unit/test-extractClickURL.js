f314e69b9f1814ad6a172b391c9a8547d06e7936
Itamar <itamar.n@matomy.com>
2015-01-11 16:15:30 +0200

    extract url

---
+++ test/unit/test-extractClickURL.js
@@ -0,0 +1,31 @@
+var fs              = require('fs'),
+    extractClickURL = require("../../src/extractClickURL");
+
+//---------------------------------------------------
+exports.testExtractGotoURL = function(test){
+    test.expect(1);
+    var html = fs.readFileSync("ads/gotourl.html",{encoding:"utf-8"});
+    extractClickURL(html,function(err,clickURL){
+        test.equal(clickURL,'https://play.google.com/store/apps/details?id=com.topgames.FighterCowboy.en');
+        test.done();
+    });
+};
+//---------------------------------------------------
+exports.testExtractDataClickURL = function(test){
+    test.expect(1);
+    var html = fs.readFileSync("ads/data-clickurl.html",{encoding:"utf-8"});
+    extractClickURL(html,function(err,clickURL){
+        test.equal(clickURL,'https://play.google.com/store/apps/details?id=com.topgames.FighterCowboy.en');
+        test.done();
+    });
+};
+//---------------------------------------------------
+exports.testExtractNoURL = function(test){
+    test.expect(1);
+    var html = fs.readFileSync("ads/no-url.html",{encoding:"utf-8"});
+    extractClickURL(html,function(err,clickURL){
+        test.ok(!clickURL);
+        test.done();
+    });
+};
+//---------------------------------------------------
