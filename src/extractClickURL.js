59ca6f9bdec06e514cdeb96dcf303ef3388b4d67
Itamar <itamar.n@matomy.com>
2015-01-19 14:11:01 +0200

    get clickurl from nuggets iframe

--- src/extractClickURL.js
+++ src/extractClickURL.js
@@ -1,34 +1,42 @@
-var htmlparser = require("htmlparser");
+var url         = require('url'),
+    htmlparser  = require("htmlparser");
 
 
 
 module.exports = function(html,cb){
     var handler = new htmlparser.DefaultHandler(function (error, dom) {
 
-        if(error) return cb();
+        try{
+            if(dom[0].name === 'iframe'){
+                var src = dom[0].attribs.src;
+                return cb(null,url.parse(src,true).query.overrideClickURL);
+            }
 
-        var nodes = dom,
-            node = nodes.filter(function(n){
-                return n.name === "html";
-            })[0];
+            var nodes = dom,
+                node = nodes.filter(function(n){
+                    return n.name === "html";
+                })[0];
 
-        if(node.name !== "body"){
-            node = node.children.filter(function(n){
-                return n.name === "body";
-            })[0];
+            if(node && node.name !== "body"){
+                node = node.children.filter(function(n){
+                    return n.name === "body";
+                })[0];
+            }
+            
+            if(node && node.attribs && node.attribs.onclick){
+                var m = node.attribs.onclick.match(/^gotourl\(\'(.*)\'\)$/);
+                if(m) return cb(null,m[1]);
+            }
+
+            if(node && node.attribs && node.attribs["data-clickurl"]){
+                return cb(null,node.attribs["data-clickurl"]);
+            }
+
+            cb();
         }
-        
-        if(node.attribs && node.attribs.onclick){
-            var m = node.attribs.onclick.match(/^gotourl\(\'(.*)\'\)$/);
-            if(m) return cb(null,m[1]);
+        catch(e){
+            cb(e);
         }
-
-        if(node.attribs && node.attribs["data-clickurl"]){
-            return cb(null,node.attribs["data-clickurl"]);
-        }
-
-        cb();
-
     });
     var parser = new htmlparser.Parser(handler);
     parser.parseComplete(html);
