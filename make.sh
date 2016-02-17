b2edfcb62ba563e11c872cc61a6f6d0a6b467165
Itamar Nabriski <itamar.n@matomy.com>
2016-02-17 14:30:46 +0200

    added source url for debugging

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p dist
 browserify src/index.js > dist/ad_sdk.js
+echo "//@ sourceURL= MobFox_JS_SDK.js" >> dist/ad_sdk.js
 browserify -t stringify src/native.js > dist/native_ad_sdk.js
