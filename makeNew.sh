1e73aca182671ae5b6b5052c4009a153911997c5
asafi <asaf.g@matomy.com>
2017-06-18 15:16:16 +0300

    adds src/ios-render.html + makeNew.sh html

--- makeNew.sh
+++ makeNew.sh
@@ -4,3 +4,5 @@ browserify src/new-tag.js | java -jar compiler.jar --language_in ECMASCRIPT5 > d
 browserify src/new-tag.js | java -jar compiler.jar --language_in ECMASCRIPT5 > test/www/new/tag.js
 uglifyjs -c -- src/ios-render.js > dist/ios-render.js
 uglifyjs -c -- src/ios-render.js > test/www/new/ios-render.js
+cp -f src/ios-render.html dist/ios-render.html
+cp -f src/ios-render.html test/www/new/ios-render.html
