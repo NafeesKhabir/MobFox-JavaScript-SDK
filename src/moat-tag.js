197abf489d507d42497c87ca7942a0379832d9c3
Itamar Nabriski <itamar.n@matomy.com>
2017-06-28 14:23:34 +0300

    moat fix

--- src/moat-tag.js
+++ src/moat-tag.js
@@ -1,4 +1,4 @@
-var moatFail = function(reason) {
+/*var moatFail = function(reason) {
     if (mobFoxParams) {
         if (typeof(mobFoxParams.onFail)==="function") {
             mobFoxParams.onFail(reason);
@@ -11,33 +11,31 @@ var moatSuccess = function() {
             mobFoxParams.onSuccess();
         }
     }
-};
+};*/
 window.moat_init = function(invh, bundle, json) {
     try {
         if (typeof json === 'string') {
             json = JSON.parse(json);
         }
         
-        var MOAT_DEMAND_ID      = json.request.demandPartner.id;
-        var MOAT_INVENTORY_ID   = invh;
-        var MOAT_BUNDLE_ID      = bundle;
-        
-        var classname = 'MOAT-matomygen2029925883?moatClientLevel1='+MOAT_DEMAND_ID+'&amp;moatClientLevel2='+MOAT_INVENTORY_ID+'&amp;moatClientLevel3='+MOAT_BUNDLE_ID;
-        var src = 'https://z.moatads.com/matomygen2029925883/moatad.js#moatClientLevel1='+MOAT_DEMAND_ID+'&moatClientLevel2='+MOAT_INVENTORY_ID+'&moatClientLevel3='+MOAT_BUNDLE_ID;
+        var MOAT_DEMAND_ID      = (json.request.demandPartner && json.request.demandPartner.id) || "dummy-dsp-id",
+            MOAT_INVENTORY_ID   = invh,
+            MOAT_BUNDLE_ID      = bundle,
+            classname           = 'MOAT-matomygen2029925883?moatClientLevel1='+MOAT_DEMAND_ID+'&amp;moatClientLevel2='+MOAT_INVENTORY_ID+'&amp;moatClientLevel3='+MOAT_BUNDLE_ID,
+            moatSrc             = 'https://z.moatads.com/matomygen2029925883/moatad.js#moatClientLevel1='+MOAT_DEMAND_ID+'&moatClientLevel2='+MOAT_INVENTORY_ID+'&moatClientLevel3='+MOAT_BUNDLE_ID;
         
         var noscript = document.createElement('noscript');
         noscript.className = classname;
         
-        var script  = document.createElement('script');
-        script.id   = 'mobfoxmoat';
-        script.setAttribute('type'  , 'text/javascript');
-        script.setAttribute('src'   , src);
+        var script      = document.createElement('script');
+        script.type     = 'text/javascript';
+        script.src      = moatSrc;
         
         document.body.appendChild(noscript);
         document.body.appendChild(script);
         
         moatSuccess();
     } catch(e) {
-        moatFail({err:e, src:'moat'});
+        console.log(e);
     }
-}
+};
