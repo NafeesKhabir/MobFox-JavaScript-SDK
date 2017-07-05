28e59a995e4241b9fbfc74f0b6b27c074db568fa
asafi <asaf.g@matomy.com>
2017-07-05 11:41:45 +0300

    adds src/new.js

---
+++ src/new.js
@@ -0,0 +1,254 @@
+var superagent  = require("superagent"),
+    once        = require("once"),
+    curScript   = document.currentScript;
+
+//--------------------------------------
+var DEMAND_SIZES = {
+	"300x50": {
+		"width": 300,
+		"height": 50
+	},
+	"320x50": {
+		"width": 320,
+		"height": 50
+	},
+	"300x250": {
+		"width": 300,
+		"height": 250
+	},
+	"320x480": {
+		"width": 320,
+		"height": 480
+	},
+	"480x320": {
+		"width": 480,
+		"height": 320
+	},
+	"728x90": {
+		"width": 728,
+		"height": 90
+	},
+	"90x728": {
+		"width": 90,
+		"height": 728
+	},
+	"600x1024": {
+		"width": 600,
+		"height": 1024
+	},
+	"1024x600": {
+		"width": 1024,
+		"height": 600
+	},
+	"768x1024": {
+		"width": 768,
+		"height": 1024
+	},
+	"1024x768": {
+		"width": 1024,
+		"height": 768
+	}
+};
+//--------------------------------------
+var getDist = function(point1, point2) {
+    var a = point1.width    - point2.width;
+    var b = point1.height   - point2.height;
+    return Math.sqrt( a*a + b*b );
+};
+//--------------------------------------
+var getClosestPoint = function(points, point) {
+    var min     = -1;
+    var closest = {};
+    for (key in points) {
+        var POINT = points[key];
+        if (min == -1) {
+            min     = getDist(POINT, point);
+            closest = POINT;
+            continue;
+        }
+        if (getDist(POINT, point) < min) {
+            if (POINT.width <= point.width && POINT.height <= point.height) {
+                min     = getDist(POINT, point);
+                closest = POINT;
+            }
+        }
+    }
+    return closest;
+};
+//--------------------------------------
+var timeout     = false,
+    finished    = false;
+//--------------------------------------
+var failLoad = function(reason) {
+    if (typeof(mobFoxParams.onFail)==="function") {
+        mobFoxParams.onFail(reason);
+    }
+};
+//--------------------------------------
+var successLoad = function() {
+    if (typeof(mobFoxParams.onSuccess)==="function") {
+        mobFoxParams.onSuccess();
+    }
+};
+//--------------------------------------
+var getHTML = function(json) {
+
+    var html            = json.request.htmlString,
+        markupRegExp    = new RegExp(/var markupB64\s*=\s*[\"\'](.*?)[\"\']/m),
+        matchMarkup     = json.request.htmlString.match(markupRegExp);
+
+    if (matchMarkup) {
+        html = window.atob(matchMarkup[1]);
+    }
+
+    return html;
+};
+//--------------------------------------
+var createDiv = function(json){
+
+    try {
+
+        var div = document.createElement('div');
+        div.id = "mobfoxDiv";
+
+        if(curScript && curScript.parentNode.tagName.toLowerCase() !== "head"){
+            curScript.parentNode.appendChild(div);     
+        }
+        else{
+            document.body.appendChild(div);
+        }
+
+        //css
+        div.style.border        = "none";
+        div.style.width         = mobFoxParams.adspace_width + "px";
+        div.style.height        = mobFoxParams.adspace_height + "px";
+        div.style.overflow      = "hidden";
+        div.style.margin        = "0px";
+        div.style.padding       = "0px";
+        div.style.display       = "inline-block";
+
+        var html = getHTML(json); 
+        div.innerHTML = html;
+
+        finished = true; 
+        successLoad(); 
+
+    }
+    catch(e) {
+        finished = true;
+        failLoad({e1:e});
+    }
+
+};
+//--------------------------------------
+var createIFrame = function(json){
+    
+    try{
+        var ifrm = document.createElement('iframe');
+        ifrm.id = "mobfoxFrame";
+
+        if(curScript && curScript.parentNode.tagName.toLowerCase() !== "head"){
+            curScript.parentNode.appendChild(ifrm);     
+        }
+        else{
+            document.body.appendChild(ifrm);
+        }
+
+        //css
+        ifrm.frameborder = "0";
+        ifrm.style.border    = "none";
+        ifrm.style.width     = mobFoxParams.adspace_width + "px";
+        ifrm.style.height    = mobFoxParams.adspace_height + "px";
+        ifrm.style.overflow  = "hidden";
+        ifrm.style.margin    = "none";
+        ifrm.setAttribute("scrolling","no");
+
+        var html = getHTML(json); 
+
+        if(html.indexOf("<html>") < 0){
+            html = ["<html><body style='margin:0px;padding:0px;'>",html,"</body></html>"].join("\n");
+        }
+        else{
+            html = html + "<style>body{margin:0px;padding:0px}</style>";
+        }
+
+        ifrm.onload = once(function(){
+            if(timeout) return;
+            finished = true; 
+            successLoad(); 
+        });
+
+        var c = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+        
+        c.document.open();
+        c.document.write(html);
+        c.document.close();
+    }
+    catch(e) {
+        finished = true; 
+        failLoad({e2:e});
+    }
+};
+
+//--------------------------------------
+
+mobFoxParams.u          = navigator.userAgent;
+mobFoxParams.r_resp     = "json";
+mobFoxParams.rt         = "api-fetchip";
+mobFoxParams.r_type     = "banner";
+
+var url = "http://my.mobfox.com/request.php";
+
+try {
+    if (mobFoxParams.imp_secure == 1) {
+        url = "https://my.mobfox.com/request.php";
+    }
+} catch(e) {}
+
+var mobFoxCall = once(function(){
+
+    window.setTimeout(function(){
+        timeout = true;
+        if (finished) return;
+        failLoad("timeout");
+    },3000);
+    
+    superagent
+            .get(url)
+            .timeout(2500)
+            .query(mobFoxParams)
+            .end(once(function(err,resp) {
+        
+                if (timeout) return;
+                try {
+                    
+                    var problem = err || resp.error || !resp.body || resp.body.error;
+                    if (problem) {
+                        finished = true;
+                        return failLoad({e3:problem});
+                    }
+
+                    var json = resp.body;
+
+                    if (document.body) {
+                        if(mobFoxParams.noIFrame) createDiv(json);
+                        else createIFrame(json); 
+                    }
+                    else{
+                        document.addEventListener("DOMContentLoaded",function(){
+                            if(mobFoxParams.noIFrame) createDiv(json);
+                            else createIFrame(json); 
+                        });
+                    }
+                }
+                catch(e) {
+                    finished = true; 
+                    failLoad({e4:e});
+                }
+                
+            }));
+});
+
+//--------------------------------------
+
+mobFoxCall();
